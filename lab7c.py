5d983550e7af701c3cd25999a6444619b902a365
Raein Nobakht <rnobakht@myseneca.ca>
2024-07-25 19:12:16 -0400

    Finished Part of Lab7

--- lab7c.py
+++ lab7c.py
@@ -16,17 +16,31 @@ def format_time(t):
 
 def sum_times(t1, t2):
     """Add two time objests and return the sum."""
-    sum = Time(0,0,0)
-    sum.hour = t1.hour + t2.hour
-    sum.minute = t1.minute + t2.minute
-    if sum.minute >= 60:
-        sum.hour = sum.hour + 1
-        sum.minute = sum.minute - 60
-    sum.second = t1.second + t2.second
-    if sum.second >= 60:
-        sum.minute = sum.minute + 1
-        sum.second = sum.second - 60
-    return sum
+    timeaddedinsec = time_to_sec(t1) + time_to_sec(t2)
+    time = sec_to_time(timeaddedinsec)
+    return time
+
+def change_time(time, seconds):
+    timeinsec = time_to_sec(time)
+    calculations = timeinsec + seconds
+    timex = sec_to_time(calculations)
+    time.hour = timex.hour
+    time.minute = timex.minute
+    time.second = timex.second
+    return None
+
+def time_to_sec(time):
+    '''convert a time object to a single integer representing the number of seconds from mid-night'''
+    minutes = time.hour * 60 + time.minute
+    seconds = minutes * 60 + time.second
+    return seconds
+
+def sec_to_time(seconds):   
+    '''convert a given number of seconds to a time object in hour,minute,second format'''
+    time = Time()
+    minutes, time.second = divmod(seconds, 60)
+    time.hour, time.minute = divmod(minutes,60)
+    return time
 
 def valid_time(t):
     """check for the validity of the time object attributes:
--- lab7d.py
+++ lab7d.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Student ID: [seneca_id] 
+# Student ID: 103021226
 class Time:
     """Simple object type for time of the day.
         data attributes: hour, minute, second
@@ -19,10 +19,9 @@ class Time:
 
     def sum_times(self, t2):
         """Add two time objests and return the sum."""
-        self_sec = self.time_to_sec()
-        t2_sec = t2.time_to_sec()
-        sum = sec_to_time(self_sec + t2_sec)
-        return sum
+        timeaddedinsec = time_to_sec(t1) + time_to_sec(t2)
+        time = sec_to_time(timeaddedinsec)
+        return time
 
     def change_time(self, seconds):
         time_seconds = self.time_to_sec()
